b1b37f029c92d3b53892f398e35ec858ffbe7458
CarlosFOL <carlosdsscience@gmail.com>
2025-07-31 18:46:12 +0200

    It was added the nested loop to iterate over the weather data for each day

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from dotenv import load_dotenv
 import json
 import os
@@ -9,6 +10,8 @@ API_KEY = os.getenv("API_KEY")
 STR_REQ = "https://servizos.meteogalicia.gal/apiv4/"
 
 format_json = lambda response : json.loads(response.content.decode())
+format_isodate = lambda date: (
+    datetime.fromisoformat(date).strftime("%Y-%m-%d %H:%M:%S").split())
 
 
 class WeatherForecastService:
@@ -52,5 +55,21 @@ class WeatherForecastService:
         """
         Process the JSON file containing the temperature, wind, and
         precipitation forecasts for the next few days.
+
+        Args:
+            data: dict
+                JSON obtained from the API request.
         """
-        pass
+        # The weather data for each day.
+        wdata_day: list = data["features"][0]["properties"]["days"]
+
+        db_wdata = [] # Store each record for each day, variable and value.
+
+        # Iterate over the variables (temperature, wind and precipitation amount)
+        for d in wdata_day:
+            for var in d["variables"]:
+                for v in var["values"]: # The variable values for each day
+                    date, time = format_isodate(v["timeInstant"])
+                    db_wdata.append((var["name"], date, time, v["value"]))
+
+        return db_wdata
